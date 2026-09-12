7bd992bbd72c36739224d13f19ce9203cf28d6bb
Ardana <79736227+ardanarx@users.noreply.github.com>
2026-09-13 00:41:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/* Ardanarx public-site configuration. Never place a service-role key here. */
+window.ARDANARX_CONFIG = Object.freeze({
+    supabaseUrl: "https://isqimtsaeuvceijapddj.supabase.co",
+    supabasePublishableKey: "sb_publishable_SrJsbjs-_hunnBhCTlKywA_JxtScx3t"
+});
