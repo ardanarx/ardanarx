c28478386220149c5c84f25a82eaa8a1e813599a
Ardana <79736227+ardanarx@users.noreply.github.com>
2026-09-13 10:24:28 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* Ardanarx public-site configuration. Never place a service-role key here. */
-window.ARDANARX_CONFIG = Object.freeze({
-    supabaseUrl: "https://isqimtsaeuvceijapddj.supabase.co",
-    supabasePublishableKey: "sb_publishable_SrJsbjs-_hunnBhCTlKywA_JxtScx3t"
-});
